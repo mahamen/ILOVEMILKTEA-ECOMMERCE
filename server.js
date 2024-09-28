c70109614861c9d065a00c4bb9c3b576585a3574
Maria Hanah G. Mendoza <161846441+mahamen@users.noreply.github.com>
2024-09-28 13:44:01 +0800

    First Commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the public folders
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/lib', express.static(path.join(__dirname, 'lib')));
+
+// Define routes for each HTML page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+app.get('/faq', (req, res) => {
+    res.sendFile(path.join(__dirname, 'faq.html'));
+});
+
+app.get('/gallery', (req, res) => {
+    res.sendFile(path.join(__dirname, 'gallery.html'));
+});
+
+app.get('/help', (req, res) => {
+    res.sendFile(path.join(__dirname, 'help.html'));
+});
+
+app.get('/product', (req, res) => {
+    res.sendFile(path.join(__dirname, 'product.html'));
+});
+
+app.get('/service', (req, res) => {
+    res.sendFile(path.join(__dirname, 'service.html'));
+});
+
+app.get('/sign-in', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sign-in.html'));
+});
+
+app.get('/sign-up', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sign-up.html'));
+});
+
+app.get('/userprofile', (req, res) => {
+    res.sendFile(path.join(__dirname, 'userprofile.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
